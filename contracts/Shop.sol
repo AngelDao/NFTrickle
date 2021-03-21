ace7a3d6bf59008220658d12ae0992141d38872b
DanWizard <tortortorented@gmail.com>
2021-03-20 22:18:18 -0500

    shop contract

---
+++ contracts/Shop.sol
@@ -0,0 +1,32 @@
+pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract Shop {
+    using SafeMath for uint256;
+
+    address NFT;
+    address owner;
+    uint256 price;
+    uint256 endBlock;
+    address tokenToPayIn;
+
+    constructor(
+        address _NFT,
+        address _owner,
+        uint256 _price,
+        uint256 _endBlockBuffer,
+        address _tokenToPayIn
+    ) {
+        NFT = _NFT;
+        owner = _owner;
+        price = _price;
+        endBlock = block.number.add(_endBlockBuffer);
+        tokenToPayIn = _tokenToPayIn;
+    }
+
+    function claim(uint256 _amount, uint256 _pid) public {
+        require(block.number >= endblock, "Not yet :)");
+    }
+
+    function buy() public {}
+}
